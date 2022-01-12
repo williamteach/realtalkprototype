1a3ac4b984e21e8368adbd4f1743b3978da40902
Claus Nomi <senoraraton@gmail.com>
2022-01-12 01:48:18 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const Pool = require("pg").Pool;
+var passwd   = process.env.RT_DATABASE;
 
 const pool = new Pool(
     {
     user: "postgres",
-    password: "R@TW00Dbull",
+    password: passwd,
     database: "rt_database",
     host: "localhost",
     port: 5432
